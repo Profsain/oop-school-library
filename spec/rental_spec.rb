24d5931f905cdd565f768e8a8082d89420ce8d1d
Profsain <profsaincrown@gmail.com>
2022-09-08 06:52:53 +0100

    Add classroom and rental unit test

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -1,5 +1,10 @@
 require_relative  '../rental.rb'
 
   describe Rental do
-
+    it 'Create a new rental' do	
+      @rental = Rental.new('2022-01-01', 'John Doe', 'The Hobbit')
+      expect(@rental.date).to eq('2022-01-01')
+      expect(@rental.person).to eq('John Doe')
+      expect(@rental.book).to eq('The Hobbit')
+    end
   end
